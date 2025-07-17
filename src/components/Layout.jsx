fa2aa19bffaae49f18e2fafc9273be09ced65424
Sugatraj <sugatraj.2106@gmail.com>
2025-07-17 20:33:42 +0530

    Comment out user menu and notification bell section in Layout component for future adjustments.

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -258,11 +258,11 @@ export default function Layout({ children }) {
             </div>
 
             {/* User menu and notification bell */}
-            <div className="flex items-center gap-4">
+            {/* <div className="flex items-center gap-4">
               <NotificationBell />
               
            
-            </div>
+            </div> */}
           </div>
           
           {/* Mobile Date Filter and Refresh Button - only visible on mobile */}
