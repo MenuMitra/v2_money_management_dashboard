0fe071b2df0db5400740c7d7b542178a72aa8e2a
shweta266 <shwetasathe2024@gmail.com>
2025-09-12 16:36:41 +0530

    added changes

--- src/pages/Statistics.jsx
+++ src/pages/Statistics.jsx
@@ -207,7 +207,7 @@ const CollectionSourcesCard = ({ collectionData }) => {
       name: 'Card',
       amount: data.card_amount || 0,
       orders: data.card_orders || 0,
-      color: 'bg-blue-500'
+      color: 'bg-yellow-500'
     },
     {
       name: 'Complementary',
@@ -216,28 +216,23 @@ const CollectionSourcesCard = ({ collectionData }) => {
       color: 'bg-pink-500'
     },
     {
-      name: 'Udhari',
+      name: 'Udhari Settle',
       amount: data.udhari_amount || 0,
       orders: data.udhari_orders || 0,
-      color: 'bg-yellow-500'
+      color: 'bg-green-500'
     },
     {
       name: 'Advance Payment',
       amount: data.advance_payment_amount || 0,
       orders: data.advance_payment_orders || 0,
-      color: 'bg-green-500'
+      color: 'bg-indigo-500'
     },
-    {
-      name: 'Udhari Settle',
-      amount: data.udhari_paid_amount || 0,
-      orders: data.udhari_paid_orders || 0,
-      color: 'bg-green-500'
-    },
+    
     {
       name: 'Udhari Pending',
       amount: data.udhari_pending_amount || 0,
       orders: data.udhari_pending_orders || 0,
-      color: 'bg-green-500'
+      color: 'bg-red-500'
     }
   ];
 
