967eb259d3f88482ed2582abadf9a92c1c38994d
Santosh <santosh@example.com>
2025-11-06 15:21:36 +0530

    changes

--- src/pages/Statistics.jsx
+++ src/pages/Statistics.jsx
@@ -579,6 +579,8 @@ const CollectionSourcesCard = ({ collectionData }) => {
     udhari_paid_amount: 0,
     udhari_pending_orders: 0,
     udhari_paid_orders: 0,
+    unknown_payment_amount: 0,
+    unknown_payment_orders: 0,
     other_amount: 0,
     other_orders: 0,
   };
@@ -640,6 +642,12 @@ const CollectionSourcesCard = ({ collectionData }) => {
       amount: data.udhari_pending_amount || 0,
       orders: data.udhari_pending_orders || 0,
       color: "bg-red-500",
+    },
+    {
+      name: "Unknown Payment",
+      amount: data.unknown_payment_amount || 0,
+      orders: data.unknown_payment_orders || 0,
+      color: "bg-gray-500",
     },
   ];
 
